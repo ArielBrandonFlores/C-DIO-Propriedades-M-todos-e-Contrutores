ce676248965d960e9a9178d31fd62a1d0642b9a5
ArielBrandonFlores <brandon.giron.flores@gmail.com>
2023-12-28 22:09:12 -0300

    Exercicio de Tratamento de Execeções com Trow e Try Catch

--- Program.cs
+++ Program.cs
@@ -1,21 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-using C_DIO_Propriedades_Métodos_e_Contrutores.Models;
+﻿using C_DIO_Propriedades_Métodos_e_Contrutores.Models;
+Console.WriteLine("--------------------Tratamento de Exceções do tipo Throw------------------------------");
+
+new ExemploExcecao().Metodo1();
+
+//Tratamento de Execoções
+Console.WriteLine("-------------------------Tratamento de Exceções do tipo Try Catch------------------");
+try
+{
+    String[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+//Excessoes Especificas
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Diretorio não encontrado: {ex.Message}");
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Arquivo não encontrado: {ex.Message}");
+}
+//Excessoes Genericas
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma Excessão generica: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Chegou até aqui");
+
+}
 
 
-Console.WriteLine("Hello, World!");
-Pessoa pessoa = new Pessoa("Ariel", "Brandon");
 
-Pessoa pessoa1 = new Pessoa();
-pessoa1.Nome = "Mathias";
-pessoa1.Sobrenome = "Flores";
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionarAluno(pessoa);
-cursoDeIngles.AdicionarAluno(pessoa1);
-cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Hello, World!");
+// Pessoa pessoa = new Pessoa("Ariel", "Brandon");
+
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Mathias";
+// pessoa1.Sobrenome = "Flores";
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(pessoa);
+// cursoDeIngles.AdicionarAluno(pessoa1);
+// cursoDeIngles.ListarAlunos();
 
 
 
